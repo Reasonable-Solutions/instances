d41d1f97edb9d46a38e7152db65c9c4d0605ba23
carl <carl.hedgren@gmail.com>
2017-06-26 00:23:08 +0200

    start adding steit, which is exactly state
    don't forget to implement put, get etc.

---
+++ src/Steit.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE InstanceSigs #-}
+module Steit where
+
+newtype Steit s a =
+  Steit { runSteit :: s -> (a, s) }
+
+instance Functor ( Steit s) where
+  fmap :: (a -> b) -> Steit s a -> Steit s b
+-- Steit s :: s -> (a, s)
+  fmap f (Steit f') = Steit b'
+    where b' = (\(a,s) -> (f a, s)) . f'
+    -- wtf did i just do here?
